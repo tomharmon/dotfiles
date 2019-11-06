6cc8e4ece2fc41f84f3b292b2edfd354e45c29b5
thomas harmon <thomas.alan.harmon@gmail.com>
2019-11-05 20:26:59 -0500

    Give colors to tmux

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -3,5 +3,6 @@ abbr -a gic 'git commit'
 abbr -a ll 'ls -la'
 abbr -a gil 'git log --all --decorate --oneline --graph'
 
-set fish_prompt_pwd_dir_length 0
+#set fish_prompt_pwd_dir_length 0
+set -x TERM "xterm-256color"
 set -U set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/bin /bin
