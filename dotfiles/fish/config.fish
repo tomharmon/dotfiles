d75dccef250f708f0c01fd50754822acf17d8efc
Thomas Harmon <thomas.alan.harmon@gmail.com>
2019-12-12 12:23:55 -0500

    add aliases for exa

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,7 +1,8 @@
 abbr -a gia 'git add -u'
 abbr -a gic 'git commit'
-abbr -a ll 'ls -la'
 abbr -a gil 'git log --all --decorate --oneline --graph'
+abbr -a ls 'exa'
+abbr -a ll 'exa -la -T -L 3'
 
 #set fish_prompt_pwd_dir_length 0
 set -x TERM "xterm-256color"
