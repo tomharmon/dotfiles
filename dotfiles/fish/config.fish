cc8a18a5e6505038f8cb95430aecf5cf43fcd1ce
Thomas Harmon <thomas.alan.harmon@gmail.com>
2019-12-28 21:43:30 -0800

    Add git aliases for reset, status

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,9 +1,16 @@
 abbr -a gia 'git add -u'
 abbr -a gic 'git commit'
+abbr -a gip 'git pull --rebase'
 abbr -a gil 'git log --all --decorate --oneline --graph'
-abbr -a ls 'exa'
-abbr -a ll 'exa -la -T -L 3'
+abbr -a gis 'git status'
+abbr -a gir 'git reset'
+abbr -a ls 'exa -F'
+abbr -a la 'exa -la'
+abbr -a lt 'exa -laT -L 3 -I .git'
 
+source $HOME/.cargo/env
 #set fish_prompt_pwd_dir_length 0
 set -x TERM "xterm-256color"
-set -U set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/bin /bin
+set -U set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/bin /bin ~/.cargo/bin
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
